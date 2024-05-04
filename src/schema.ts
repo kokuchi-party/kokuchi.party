e7135f29d4b4e1d04262f8754ec87bd2f42fd521
cannorin <cannorin@users.noreply.github.com>
2024-05-04 16:30:46 +0900

    Edit src/schema.ts

--- src/schema.ts
+++ src/schema.ts
@@ -2,6 +2,5 @@ import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
 export const posts = sqliteTable("posts", {
   id: integer("id").primaryKey().notNull(),
-  title: text("title").notNull(),
   content: text("content")
 });
