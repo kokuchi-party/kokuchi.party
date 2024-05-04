2abede921fd8655c0dd86181f80080e3ef96aa43
cannorin <cannorin@users.noreply.github.com>
2024-05-04 17:03:08 +0900

    Add /posts

---
+++ src/routes/posts/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<h1>Posts</h1>
+
+<ul>
+  {#each data.posts as item}
+    <li>
+      [{item.id}] {item.content}
+    </li>
+  {/each}
+</ul>
