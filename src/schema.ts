801f99edb954c31707e48e24083c87b90c9e2bab
cannorin <cannorin@users.noreply.github.com>
2024-05-04 17:14:25 +0900

    Remove /posts

--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,1 @@
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
-
-export const posts = sqliteTable("posts", {
-  id: integer("id").primaryKey().notNull(),
-  content: text("content")
-});
+// import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
