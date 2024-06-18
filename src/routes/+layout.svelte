5815b3d21816fe2762e6a0b8e3f07add9e91961d
cannorin <cannorin@users.noreply.github.com>
2024-06-18 15:30:58 +0900

    Add mode-watcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,12 @@
   import "@fontsource-variable/orbitron";
   import "../app.pcss";
 
+  import { ModeWatcher } from "mode-watcher";
   import { setLanguageTag } from "$paraglide/runtime";
 
   export let data;
   $: setLanguageTag(data.lang);
 </script>
 
-<slot></slot>
+<ModeWatcher />
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
+  import { toggleMode } from "mode-watcher";
   import type { PageData } from "./$types";
 
   export let data: PageData;
 </script>
 
 <header>
-  <nav>
+  <nav class="flex flex-col items-start">
     {#if data.user}
       <a href="/user/logout">Signout</a>
     {:else}
       <a href="/user/login">Sign In</a>
     {/if}
+    <button on:click={toggleMode}> Toggle Theme </button>
   </nav>
 </header>
 
